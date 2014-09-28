4cc553f7a0524f188d97ddd9d87b7046ee056164
Crissfade <jarvis@host.local>
2014-09-28 17:26:17 +0300

    1

---
+++ Chapter 5/Chapter 5/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Chapter 5
+//
+//  Created by jarvis on 9/28/14.
+//  Copyright (c) 2014 jarvis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
